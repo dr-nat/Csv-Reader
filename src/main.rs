3f10e3e1958d429cfba2915b7708794b345c0939
Nathaniel Parah <nathanielparah579@gmail.com>
2026-01-22 12:16:06 +0100

    Added the csv crate, added a practice file, and created an initial implementation of the csv reader
    Signed-off-by: Nathaniel Parah <nathanielparah579@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,45 @@
 use std::env; 
 use std::error::Error;
-
+use std::io::{BufRead, BufReader};
+use std::fs::File;
+use csv::Reader;
 
 struct CsvRows {
-    header: Vec<String>,
+    header: Vec<String>,        
     rows: Vec<Vec<String>>,
 }
 
 
-fn parser_args() -> Result<(), Box<dyn Error>>{
+fn parse_args() -> Result<(), Box<dyn Error>>{
    let args: Vec<String> = env::args().collect();
 
-   let _first_arg = &args[1];
 
    if args.len() < 2 {
-       return Err(format!("Error:No arguments were provided").into());
+       return Err(format!("No arguments (filepaths/name) were provided").into());
    }
+   
+   let first_arg = &args[1];
+    
+   let mut reader = Reader::from_path(first_arg)?;
 
+   let header = reader.headers()?;
+   println!("{:?}", header);
+
+   for rows in reader.records() {
+        let records = rows?;
+        println!("{:?}", records);
+   }
    Ok(())
 }
 
 
 fn main() {
-    println!("Hello, world!");
+    let csv_reader = parse_args();
+
+    match csv_reader {
+        Ok(value) => value,
+        Err(e) => {
+            eprintln!("Error: {}", e);
+        }
+    } 
 }
